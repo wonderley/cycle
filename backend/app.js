96dec1a388402fd9a8c44f8ce8833bf1f24940e0
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2020-12-01 15:28:13 -0500

    backend: capture stdin

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,27 @@
 const Model = require('./model')
 const { CreateHandler } = require('./handler')
+const readline = require('readline');
 
 if (require.main === module) {
+  main()
+}
+
+function main() {
   const model = new Model()
   const handler = CreateHandler(model)
   const port = 4000
   handler.listen(port, () => {
     console.log(`Listening on port ${port}`)
+  })
+
+  // stdin
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+  
+  rl.on('line', function(line){
+    console.log(line);
   })
 }
