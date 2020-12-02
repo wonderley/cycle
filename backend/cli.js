3b81bc3ebbbbc3d15be3eea324aa33c59776a34d
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2020-12-01 22:41:43 -0500

    improve output for list, add

--- backend/cli.js
+++ backend/cli.js
@@ -7,23 +7,25 @@ function Interpret(text, model=fileModel) {
   const cmd  = splitText[0],
         args = splitText.splice(1)
   if (cmd === 'list') {
-    return model.GetTasks()
-  }
-  if (cmd === 'add') {
+    const tasks = model.GetTasks()
+    return tasks.map(t =>
+      `${t.id.substring(0, 4)} ${t.desc}`
+    ).join('\n')
+  } else if (cmd === 'add') {
     if (!args.length) return 'nothing to add'
     const task = {
       desc: args.join(' '),
     }
     const result = model.AddTask(task)
-    return `added task: ${JSON.stringify(result)}`
-  } else if (cmd === 'done') {
+    return `➕${result.id.substring(0, 4)}`
+  } else if (cmd === 'fin') {
     if (!args.length) return 'specify a task to complete'
     if (args.length === 1) {
       const possibleId = args[0]
       if (possibleId.length > 2
        && possibleId.every(c => c.match(/[0-9a-f]/))) {
         // The argument could be the task ID
-        // const res = model.removeById(possibleId)
+        // const res = model.RemoveById(possibleId)
       }
     }
   } else {
